36fa9806ce398fc076dd1404ef5e52e45d4e728b
Mark 'Grandy' Bishop <mgrandyb@gmail.com>
2023-10-10 20:49:46 +0100

    remove original template loader

--- common/graphic.js
+++ common/graphic.js
@@ -29,17 +29,6 @@ function getAndCheckURLParam(param) {
 	}
 	return p;
 }
-function loadTemplate(name) {
-	console.log(`Loading template: '${name}'`);
-	var scriptElement = document.createElement("script");
-	scriptElement.src = `./${name}/script.js`;
-	document.head.appendChild(scriptElement);
-	scriptElement.onload = function () {
-		load();
-	};
-
-	addStylesheet(`./${name}/styles.css`);
-}
 
 const tabName = getAndCheckURLParam("tab");
 const spreadsheetId = getAndCheckURLParam("id");
