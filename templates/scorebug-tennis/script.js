2f2e8ab7a1f1969cfa6bad9f60b712e53e84034d
Mark Bishop <mgrandyb@gmail.com>
2024-09-28 12:27:23 +0100

    tennis scorebug

---
+++ templates/scorebug-tennis/script.js
@@ -0,0 +1,40 @@
+// MIT License
+//
+// Copyright (c) 2023 Mark "Grandy" Bishop
+// https://github.com/GrandyB/SheetToHTML
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+function load() {
+    var dom = document.getElementById("main");
+    var html = "";
+    
+    html += `<div class="cont">`
+    html += `<div class="scorebug">`;
+    
+    html += `<div class="col names"><div id="C2" apply-as-classes="C20"></div><div id="D2" apply-as-classes="D20"></div></div>`;
+    for (var i = 3; i <= 7; i++) {
+        html += `<div class="col set" apply-as-classes="G${i}"><div id="C${i}"></div><div id="D${i}"></div></div>`;
+    }
+    html += `<div class="col points"><div id="C12"></div><div id="D12"></div></div>`;
+
+    html += `</div></div>`;
+    
+    dom.innerHTML += html;
+}
