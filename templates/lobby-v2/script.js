f3dbe4f12578402dc47f13af71ae5bc950945e11
Mark Bishop <mgrandyb@gmail.com>
2024-10-25 10:26:01 +0100

    move civ snipe/ban size change to being behind &big-icons, fix name sizes/wrapping

--- templates/lobby-v2/script.js
+++ templates/lobby-v2/script.js
@@ -140,12 +140,12 @@ function load() {
     html += `</div>`;
 
     // NAMES AND SCORES
-    html += partialBoxRight({ class: 'player left name', style: `left: 170px; top: 64px; width: 400px; height: 90px;`, content: '<span id="B1"></span>'});
+    html += partialBoxRight({ class: 'player left name', style: `left: 20px; top: 64px; width: 550px; height: 90px;`, content: '<span id="B1"></span>'});
     html += partialBoxCentered({ class: 'player left score', style: `left: 621px; top: 72px; width: 90px; height: 90px;`, content: '<span id="D1"></span>'});
     html += partialBoxCentered({ class: 'player left country', style: `left: 621px; top: 8px; width: 90px; height: 90px;`, content: '<img id="M19" />'});
     html += partialBoxCentered({ class: 'player right country', style: `left: 908px; top: 8px; width: 90px; height: 90px;`, content: '<img id="M20" />'});
     html += partialBoxCentered({ class: 'player right score', style: `left: 908px; top: 72px; width: 90px; height: 90px;`, content: '<span id="D2"></span>'});
-    html += partialBoxLeft({ class: 'player right name', style: `left: 1042px; top: 64px; width: 400px; height: 90px;`, content: '<span id="B2"></span>'});
+    html += partialBoxLeft({ class: 'player right name', style: `left: 1042px; top: 64px; width: 550px; height: 90px;`, content: '<span id="B2"></span>'});
 
     // ROUND INFO
     html += `<div class="round-info">`;
