9c8b4810e27506cfb4b9750cf0a00ebe7098f60a
Mark Bishop <mgrandyb@gmail.com>
2024-11-03 14:58:19 +0000

    flags and names in ingame

--- templates/ingame-score/script.js
+++ templates/ingame-score/script.js
@@ -35,10 +35,22 @@ function load() {
     let mapLeft = getAndCheckURLParam("mapLeft");
     let mapTop = getAndCheckURLParam("mapTop");
 
+    let leftNameLeft = getAndCheckURLParam("lnLeft");
+    let leftNameTop = getAndCheckURLParam("lnTop");
+    let leftNameFlagLeft = parseInt(leftNameLeft) + 200;
+    let rightNameLeft = getAndCheckURLParam("rnLeft");
+    let rightNameTop = getAndCheckURLParam("rnTop");
+    let rightNameFlagLeft = parseInt(rightNameLeft) - 40;
+
     // Scores
     html += partialBoxCentered({ class: 'left score', style: `left: ${leftScoreLeft}px; top: ${scoreTop}px; width: 85px; height: 85px;`, content: `<span id="B2"></span>`}); // 814
     html += partialBoxCentered({ class: 'right score', style: `left: ${rightScoreLeft}px; top: ${scoreTop}px; width: 85px; height: 85px;`, content: `<span id="B3"></span>`}); // 1022
     html += partialBoxCentered({ class: 'bestof', style: `left: ${bestOfLeft}px; top: ${bestOfTop}px; width: 270px; height: 50px;`, content: `<span id="B5"></span>`}); // 822
+
+    html += partialBoxRight({ class: 'left name', style: `left: ${leftNameLeft}px; top: ${leftNameTop}px; width: 200px; height: 24px;`, content: `<span id="B14"></span>`});
+    html += partialBoxCentered({ class: 'left flag', style: `left: ${leftNameFlagLeft}px; top: ${leftNameTop}px; width: 40px; height: 24px;`, content: `<img id="B16"></img>`});
+    html += partialBoxLeft({ class: 'right name', style: `left: ${rightNameLeft}px; top: ${rightNameTop}px; width: 200px; height: 24px;`, content: `<span id="B18"></span>`});
+    html += partialBoxCentered({ class: 'left flag', style: `left: ${rightNameFlagLeft}px; top: ${rightNameTop}px; width: 40px; height: 24px;`, content: `<img id="B20"></img>`});
 
     html += `<div requires-non-empty="B7" style="position: absolute; width: 400px; left: ${mapLeft}px; top: ${mapTop}px;">`
         html += `<div id='map-container'>`;
